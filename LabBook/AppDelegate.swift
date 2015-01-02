aa75e479810bfa6024a88e6147ac35282add41b9
HL <Hongjin@hongjins-mbp.home>
2015-01-02 22:52:13 +0000

    Mac OS
    Create a new project.

---
+++ LabBook/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LabBook
+//
+//  Created by Hongjin Lu on 02/01/2015.
+//  Copyright (c) 2015 Hongjin Lu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LabBook/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LabBook
+//
+//  Created by Hongjin Lu on 02/01/2015.
+//  Copyright (c) 2015 Hongjin Lu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
